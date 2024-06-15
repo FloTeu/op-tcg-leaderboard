d79e567e0c61652202f00b2e049c9227ba70a11a
FloTeu <37250993+FloTeu@users.noreply.github.com>
2024-06-15 12:33:23 +0200

    Fix Leaderboard mobile view (#23)
    * Feature/14 terraform (#19)
    
    * feat: Add terraform code to setup cloud infrastructure #14
    
    * feat: Add more logging information #14
    
    * feat: add terraform variables #14
    
    * feat: add cloud scheduler to terraform #14
    
    * feat: fix terraform iam issues #14
    
    * feat: fix terraform iam issues #14
    
    * fix: increase timeout limit #14
    
    * fix: use pub/sub for function call #14
    
    * fix: Make cloud function scalable #14
    
    * fix: pub/sub bug without return statement #14
    
    * feat: merge main
    
    * fix: elo bq insert
    
    * fix: pydantic model parsing
    
    * feat: terraform code for tournament crawling
    
    * fix: allow placing None for tournament standing
    
    * refactor: Optimize performance of leaderboard
    
    * refactor: add init load data function
    
    * refactor: tournament winner table
    
    * feat: increase performance of meta analysis page
    
    * feat: connect leaderboard and decklist page with hyperlinks
    
    * fix: order for Leaders without tournament wins
    
    * fix: upload match bug
    
    * fix: change rule for marking tournaments as official
    
    * feat: add players decklists view
    
    * feat: improve stylings
    
    * feat: add is_mobile function
    
    * feat: fix leaderboard for mobile view

---
+++ op_tcg/frontend/utils/js.py
@@ -0,0 +1,38 @@
+import time
+import random
+
+#from streamlit_js_eval import streamlit_js_eval
+from functools import cache
+from uuid import uuid4
+from op_tcg.frontend.utils.session import get_session_id
+
+
+@cache
+def get_screen_width(session_id: str) -> int | None:
+    """
+    Return screen width by javascript call
+    st_session_id is provided to ensure screen width is detected once per user (because of cache)
+
+    raises:
+        TypeError: If no screen width can be detected
+
+    """
+
+    # Note: Can be none, if javascript is not able to detect browser width -> will throw an exception
+    try:
+        screen_width = streamlit_js_eval(js_expressions='screen.width', want_output=True, key=f"SCR")
+        return int(screen_width)
+    except Exception as e:
+        print(e)
+        time.sleep(random.randint(0,2))
+        screen_width = streamlit_js_eval(js_expressions='screen.width', want_output=True, key=f"SCR{uuid4().hex}")
+        return int(screen_width)
+
+
+def is_mobile() -> bool:
+    try:
+        width: int = get_screen_width(get_session_id())
+    except TypeError:
+        # Thrown if screen width is not know yet
+        return False
+    return width < 600
