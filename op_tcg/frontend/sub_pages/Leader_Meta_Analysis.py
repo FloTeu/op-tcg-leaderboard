8c4e99de302b18b169014c96cf8dd20a8ca713fc
fteutsch <florianteutsch@yahoo.de>
2024-07-21 13:56:34 +0200

    feat: remove draggable of radar plot

--- op_tcg/frontend/sub_pages/Leader_Meta_Analysis.py
+++ op_tcg/frontend/sub_pages/Leader_Meta_Analysis.py
@@ -102,8 +102,8 @@ def display_elements(selected_leader_ids,
         layout = [
             # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
             dashboard.Item("lmeta_radar_plot_item_title", 0, 0, 3, 0.5, isDraggable=False, isResizable=False),
-            dashboard.Item("lmeta_radar_plot_item", 0, 1, 11, 2, isDraggable=True, isResizable=True),
-            dashboard.Item("lmeta_avatar_group_item", 9, 0, 2, 0.5, isDraggable=True),
+            dashboard.Item("lmeta_radar_plot_item", 0, 1, 11, 2, isDraggable=False, isResizable=True),
+            dashboard.Item("lmeta_avatar_group_item", 9, 0, 2, 0.5, isDraggable=False),
             dashboard.Item("lmeta_table_item", 0, 3, 11, 6, isResizable=False, isDraggable=False),
         ]
 
