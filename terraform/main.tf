6f41f84a5d47f535a47b3a34c670bf393c6f14ee
fteutsch <florianteutsch@yahoo.de>
2024-09-28 12:49:03 +0200

    feat: add gcp credentials to terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,7 @@ terraform {
 
 
 provider "google" {
+  credentials = file(var.gcp_credentials)
   project = var.project
   region  = var.region
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,4 @@
+variable "gcp_credentials" {}
 variable "project" {}
 variable "limitless_api_token" {}
 variable "region" {
