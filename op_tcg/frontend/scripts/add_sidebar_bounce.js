6db421cce990f240b03d964a569832eb93773800
fteutsch <florianteutsch@yahoo.de>
2025-10-10 16:59:28 +0200

    feat: remove streamlit frontend code

--- op_tcg/frontend/scripts/add_sidebar_bounce.js
+++
@@ -1,7 +0,0 @@
-// Select the element with the specific data-testid attribute
-const match = parent.document.querySelector('[data-testid="stSidebarCollapsedControl"]');
-if (match) {
-    match.classList.add("bounce");
-} else {
-    console.error('Element with data-testid="stSidebarCollapsedControl" not found.');
-}
--- op_tcg/frontend/sub_pages/utils.py
+++
@@ -1,2 +0,0 @@
-def sub_page_title_to_url_path(title: str):
-    return title.replace(" ", "_").lower()
