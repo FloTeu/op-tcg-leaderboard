5535f7f595243b266d8c2fb9945906f45ae09c61
fteutsch <florianteutsch@yahoo.de>
2025-11-07 16:37:38 +0100

    fix: try to include latest htmx

--- update_htmx.sh
+++ update_htmx.sh
@@ -1,1 +1,1 @@
- curl -L https://unpkg.com/htmx.org@next/dist/htmx.min.js -o public/js/htmx.min.js && head -n 5 public/js/htmx.min.js
+ curl -L https://cdn.jsdelivr.net/npm/htmx.org@latest/dist/htmx.js -o public/js/htmx.min.js && head -n 5 public/js/htmx.min.js
